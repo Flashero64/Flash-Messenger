553eacde42799cd16706dc3310e8aea40330c8ea
Flashero64 <95875632+Flashero64@users.noreply.github.com>
2021-12-28 14:03:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 var messages = document.getElementById("messages");
 var textbox = document.getElementById("textbox");
 var button = document.getElementById("button");
-var button2 = document.getElementById("button2");
 
 button.addEventListener("click", function(){
      var newMessage = document.createElement("li");
